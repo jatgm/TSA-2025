6e2e4da17c57f8790ab614ed17e8d8417325e521
kimiya-design <natehuy08@gmail.com>
2024-12-01 14:35:23 -0800

    IE
    added THE infinity edge

--- canvas_layer.gd
+++ canvas_layer.gd
@@ -1,15 +1,17 @@
-extends Button
+extends CanvasLayer
 
-
+@export var gold = 10000
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	$Panel/TextEdit.text = str(gold)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
 
-func _on_pressed() -> void:
-	self.set_visible(false)
+func _on_button_pressed() -> void:
+	$Panel/Button.set_visible(false)
+	gold -= 5000
+	$Panel/TextEdit.text = str(gold)
+	
