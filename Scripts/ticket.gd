3ffdfc936a432645f3fd82d5ea13dd2793722751
jatgm <jatgmalt@protonmail.com>
2024-12-01 21:02:30 -0800

    720p

--- Scripts/ticket.gd
+++ Scripts/ticket.gd
@@ -4,12 +4,9 @@ extends Control
 var list_of_food = ["bread","crossaint","muffin","baguette","cupcake","cake","garlic bread","milk tea","cappucino"]
 
 func _ready() -> void:
-	$Panel/RichTextLabel.text= list_of_food[randi_range(0,list_of_food.size()-1)]
-	$AnimationPlayer.play("drop")
-	
+	$Panel/RichTextLabel.text= list_of_food[randi_range(0,list_of_food.size()-1)]	
 
-
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	position.y += y_offset
 	$Panel/ProgressBar.value -= .1
 	if $Panel/ProgressBar.value <= 0:
